c5ba74b82f811faef4a0adb5ce5754b476643a8c
Sam Boupha <samboupha@yahoo.com>
2015-01-26 14:55:17 +0530

    Initial commit stage of the project

---
+++ basic_bootstrap_template.php
@@ -0,0 +1,18 @@
+<!DOCTYPE>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="css/mycss.css">
+	</head>
+
+	<body>
+
+		<p class='btn btn-primary'>Click me</p>
+
+		//js script
+		<script type="text/javascript" src="js/jquery.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="js/myscript.js"></script>
+	</body>
+</html>
