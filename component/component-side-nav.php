b41958f833e977a8baf9a7701d942432258e3d1d
Sam Boupha <samboupha@yahoo.com>
2015-01-27 01:08:32 +0530

    changed header color to ip6 gold and adding style to nav bar

--- component/component-side-nav.php
+++ component/component-side-nav.php
@@ -1,14 +1,14 @@
-<nav class='col-md-2 text-center'>
+<nav class='col-md-2'>
 	<div class="row">
-		<h4>Computers &amp; Accessories</h4>
+		<h4>Computers &amp; Accessories <span class='pull-right glyphicon glyphicon-chevron-right'></span></h4>
 	</div>
 	<div class="row">
-		<h4>Mobiles &amp; Tablets</h4>
+		<h4>Mobiles &amp; Tablets <span class='pull-right glyphicon glyphicon-chevron-right'></h4>
 	</div>
 	<div class="row">
-		<h4>Books</h4>
+		<h4>Books<span class='pull-right glyphicon glyphicon-chevron-right'></h4>
 	</div>
 	<div class="row">
-		<h4>Clothing</h4>
+		<h4>Clothing<span class='pull-right glyphicon glyphicon-chevron-right'></h4>
 	</div>
 </nav>
--- index.php
+++ index.php
@@ -7,74 +7,72 @@
 	</head>
 
 	<body >
-	
-			<!-- header section -->
-			<?php include("component/component-header.php"); ?>
+		<!-- header section -->
+		<?php include("component/component-header.php"); ?>
+		<?php include("component/component-side-nav.php"); ?>
 
-			<?php include("component/component-side-nav.php"); ?>
+		<section  class='main-section col-md-offset-2 col-md-10'>
+			<div class="main-section-content">
+				<div id="carousel-product" class="carousel slide" data-ride="carousel">
+				  <!-- Wrapper for slides -->
+				  <div class="carousel-inner" role="listbox">
+				    <div class="item active">
+				      <img src="img/product_slides/redmi.png" alt="...">
+				    </div>
+				    <div class="item">
+				      <img src="img/product_slides/ip6.jpg" alt="...">
+				    </div>
+				    <div class="item">
+				      <img src="img/product_slides/tv.jpg" alt="...">
+				    </div>
+				  </div>
 
-			<section  class='main-section col-md-offset-2 col-md-10'>
-				<div class="main-section-content">
-					<div id="carousel-product" class="carousel slide" data-ride="carousel">
-					  <!-- Wrapper for slides -->
-					  <div class="carousel-inner" role="listbox">
-					    <div class="item active">
-					      <img src="img/product_slides/redmi.png" alt="...">
-					    </div>
-					    <div class="item">
-					      <img src="img/product_slides/ip6.jpg" alt="...">
-					    </div>
-					    <div class="item">
-					      <img src="img/product_slides/tv.jpg" alt="...">
-					    </div>
-					  </div>
-
-					  <!-- Controls -->
-					  <a class="left carousel-control" href="#carousel-product" role="button" data-slide="prev">
-					    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-					    <span class="sr-only">Previous</span>
-					  </a>
-					  <a class="right carousel-control" href="#carousel-product" role="button" data-slide="next">
-					    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-					    <span class="sr-only">Next</span>
-					  </a>
-					</div> <!-- carousel -->
-					<br /><br />
-					<div class='product-thumbnail'>
-						<h2>Computers</h2>
-						<div>
-							<img src="img/computer/apple-macbook-pro-notebook-125x125.jpeg">
-							<img src="img/computer/hp-notebook-125x125.jpeg">
-							<img src="img/computer/apple-macbook-air-notebook-125x125.jpeg">
-							<img src="img/computer/lenovo-125x125.jpeg">
-						</div>
-						<br /><br /><br /><br /><br />
-						<h2>Mobiles &amp; Tablets</h2>
-						<div>
-							<img src="img/computer/apple-macbook-pro-notebook-125x125.jpeg">
-							<img src="img/computer/hp-notebook-125x125.jpeg">
-							<img src="img/computer/apple-macbook-air-notebook-125x125.jpeg">
-							<img src="img/computer/lenovo-125x125.jpeg">
-						</div>
-						<br /><br /><br /><br /><br />
-						<h2>books</h2>
-						<div>
-							<img src="img/computer/apple-macbook-pro-notebook-125x125.jpeg">
-							<img src="img/computer/hp-notebook-125x125.jpeg">
-							<img src="img/computer/apple-macbook-air-notebook-125x125.jpeg">
-							<img src="img/computer/lenovo-125x125.jpeg">
-						</div>
-						<br /><br /><br /><br /><br />
-						<h2>Clothing</h2>
-						<div>
-							<img src="img/computer/apple-macbook-pro-notebook-125x125.jpeg">
-							<img src="img/computer/hp-notebook-125x125.jpeg">
-							<img src="img/computer/apple-macbook-air-notebook-125x125.jpeg">
-							<img src="img/computer/lenovo-125x125.jpeg">
-						</div>
-					</div> <!--	product thumbnail -->
-				</div>
-			</section> <!-- amin body section-->
+				  <!-- Controls -->
+				  <a class="left carousel-control" href="#carousel-product" role="button" data-slide="prev">
+				    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+				    <span class="sr-only">Previous</span>
+				  </a>
+				  <a class="right carousel-control" href="#carousel-product" role="button" data-slide="next">
+				    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+				    <span class="sr-only">Next</span>
+				  </a>
+				</div> <!-- carousel -->
+				<br /><br />
+				<div class='product-thumbnail'>
+					<h2>Computers</h2>
+					<div>
+						<img src="img/computer/apple-macbook-pro-notebook-125x125.jpeg">
+						<img src="img/computer/hp-notebook-125x125.jpeg">
+						<img src="img/computer/apple-macbook-air-notebook-125x125.jpeg">
+						<img src="img/computer/lenovo-125x125.jpeg">
+					</div>
+					<br /><br /><br /><br /><br />
+					<h2>Mobiles &amp; Tablets</h2>
+					<div>
+						<img src="img/computer/apple-macbook-pro-notebook-125x125.jpeg">
+						<img src="img/computer/hp-notebook-125x125.jpeg">
+						<img src="img/computer/apple-macbook-air-notebook-125x125.jpeg">
+						<img src="img/computer/lenovo-125x125.jpeg">
+					</div>
+					<br /><br /><br /><br /><br />
+					<h2>books</h2>
+					<div>
+						<img src="img/computer/apple-macbook-pro-notebook-125x125.jpeg">
+						<img src="img/computer/hp-notebook-125x125.jpeg">
+						<img src="img/computer/apple-macbook-air-notebook-125x125.jpeg">
+						<img src="img/computer/lenovo-125x125.jpeg">
+					</div>
+					<br /><br /><br /><br /><br />
+					<h2>Clothing</h2>
+					<div>
+						<img src="img/computer/apple-macbook-pro-notebook-125x125.jpeg">
+						<img src="img/computer/hp-notebook-125x125.jpeg">
+						<img src="img/computer/apple-macbook-air-notebook-125x125.jpeg">
+						<img src="img/computer/lenovo-125x125.jpeg">
+					</div>
+				</div> <!--	product thumbnail -->
+			</div>
+		</section> <!-- amin body section-->
 			
 		<!-- js script -->
 		<script type="text/javascript" src="js/jquery.js"></script>
