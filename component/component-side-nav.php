f276ba48d2ffeaf55236394e719d5ab6f85511e8
Sam Boupha <samboupha@yahoo.com>
2015-02-02 17:36:59 +0530

    Modified side nav bar by adding <a> and style them to look normal

--- component/component-side-nav.php
+++ component/component-side-nav.php
@@ -1,14 +1,14 @@
 <nav class='col-md-2'>
-	<div class="row">
+	<a href='#'><div class="row">
 		<h4>Computers &amp; Accessories <span class='pull-right glyphicon glyphicon-chevron-right'></span></h4>
-	</div>
-	<div class="row">
+	</div></a>
+	<a href='#'><div class="row">
 		<h4>Mobiles &amp; Tablets <span class='pull-right glyphicon glyphicon-chevron-right'></h4>
-	</div>
-	<div class="row">
+	</div></a>
+	<a href='#'><div class="row">
 		<h4>Books<span class='pull-right glyphicon glyphicon-chevron-right'></h4>
-	</div>
-	<div class="row">
+	</div></a>
+	<a href='#'><div class="row">
 		<h4>Clothing<span class='pull-right glyphicon glyphicon-chevron-right'></h4>
-	</div>
+	</div></a>
 </nav>
