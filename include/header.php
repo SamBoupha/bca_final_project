fd674ba32b6cce90dfeda10c91c288c157b39bd3
Sam Boupha <samboupha@yahoo.com>
2015-02-05 00:59:35 +0530

    changed the logo

--- include/header.php
+++ include/header.php
@@ -17,7 +17,7 @@
 		<!-- header section -->
 		<header>
 				<section class="col-md-12">
-					<a href="index.php"><img src="img/mikart.png"></a>
+					<a href="index.php"><img style="height:80px;" src="img/abc-opt.png"></a>
 					<input type='text' name='search_box' placeholder='Search for a product, category or brand'>
 					<input type='button' class='btn btn-primary' name='search' value='SEARCH'>
 					<div class='account'>
