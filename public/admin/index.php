e0a44159e0ff25e5dd4a5c1b11f6ef210e6e1629
Sam Boupha <samboupha@yahoo.com>
2015-02-07 17:54:24 +0530

    created the admin page

---
+++ public/admin/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="../css/admin.css">
+	</head>
+	<body>
+		<form method='post' >
+			<h2>Admin Section</h2>
+			<img src="../img/abc.png" style=" height: 80px"><br />
+			<label>Username:</label><br />
+			<input type='text' name='username'><br />
+			<label>Password:</label><br />
+			<input type='password' name='password'><br />
+			<input type='submit'class='btn btn-primary' value='Login >>'><br />
+		</form>
+	</body>
+</html>
