d2d0a8b74f87bf306170827e1ffa5d1da6089512
Sam Boupha <samboupha@yahoo.com>
2015-02-10 18:17:26 +0530

    added CRUD features to carousel-edit.php

--- public/admin/carousel-edit.php
+++ public/admin/carousel-edit.php
@@ -9,14 +9,40 @@ if(!$session->is_logged_in()) header("location: login.php");
 
 	<section>
 		<h2>Carousel Editing</h2>
-		<a href='carousel-edit-add.php'>Add pictures</a><br />
+		<a href='carousel-edit-add.php'>Add new</a><br />
 
 		<?php
 			$dirPath = "..".DS."img".DS."product_slides";
-			$images = imgSrcAutoGen($dirPath,400);
+			$images = imgSearch($dirPath);
+			echo "<table class='table table-striped'>";
+			echo "<tr><th>Index</th><th>Sample Imgae</th><th>Link</th><th></th><th></th></tr>";
 			foreach ($images as $image) {
-				echo "<a href='index.php".$link."'>".$image."</a><br />";
+				$file_name = explode("_", $image);
+				echo "<tr>";
+				//first column - Index
+				echo "<td>";
+				echo $file_name[0];
+				echo "</td>";
+				// second column - Sample Image
+				echo "<td>";
+				echo '<img src="'.$dirPath.DS.$image.'" style= "width:400px;"></a>';
+				echo "</td>";
+				// third column - Link
+				echo "<td>";
+				echo '<a href="..'.DS.$file_name[1].'.php">'.$file_name[1].'.php</a>';
+				echo "</td>";
+				// Fourth Column - Edit
+				echo "<td>";
+				echo '<a href="carousel-edit-edit.php.php">Edit This Slide</a>';
+				echo "</td>";
+				// FifthColumn - Delete
+				echo "<td>";
+				echo '<a href="carousel-edit-delete.php"><p class="danger">Delete This Slide</p></a>';
+				echo "</td>";
+
+				echo "</tr>";
 			}
+			echo "</table>";
 		?>
 
 	</section>
