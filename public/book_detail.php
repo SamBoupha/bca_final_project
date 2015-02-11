5055d39f279eaa43fd9fec2b573b29da4d5e9b34
Sam Boupha <samboupha@yahoo.com>
2015-02-11 21:22:49 +0530

    created book_detail page

---
+++ public/book_detail.php
@@ -0,0 +1,9 @@
+<?php include("../include/initialize.php"); ?>
+<?php $subpage = true; ?>
+<?php include(INC_PATH.DS."header.php"); ?>
+
+<section>
+		
+</section> <!-- main body section-->
+
+<?php include(INC_PATH.DS."footer.php"); ?>
