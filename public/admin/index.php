46df4312e75e495acf94562677808004d527934a
Sam Boupha <samboupha@yahoo.com>
2015-02-11 17:40:50 +0530

    Added style for admin <section>

--- public/admin/index.php
+++ public/admin/index.php
@@ -7,7 +7,9 @@ if(!$session->is_logged_in()) header("location: login.php");
 <?php include(INC_PATH.DS.'side-nav-admin.php');?>
 
 	<section>
-		<h2>Hello <?php echo $session->admin_username; ?>!. Welcome to the Admin area</h2>
+		<div class='index'>
+			<h2>Hello <em><?php echo $session->admin_username; ?></em>!. Welcome to the Admin area</h2>
+		</div>
 	</section>
 
 <?php include(INC_PATH.DS.'footer-admin.php');?>
