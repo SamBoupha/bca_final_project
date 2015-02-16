ca41eaa37c277904e981939809757f43532898cb
Sam Boupha <samboupha@yahoo.com>
2015-02-16 23:38:17 +0530

    moved instanciate and get_column_name functions to db_object

--- include/book_object.php
+++ include/book_object.php
@@ -93,63 +93,6 @@ class BookObject extends DatabaseObject {
 		return self::instanciate($sql);
 	}
 
-	protected static function get_column_names($sql) {
-		global $db;
-		if (!$db->is_connected()) {
-			$db->open_connection();
-		}
-
-		$db_rows = array();
-		// The idea here is to reduce the number of times needed to go to the database
-		// So the work-around is to use one associative array that contains two arrays
-		$result_n_fieldName = array();
-
-		$result = $db->execute_query($sql);
-		// This assoc 'result' will contain the array of rows of result 
-		// retrieved from the database
-		while ($row = $db->db_fetch_assoc($result)) {
-			$db_rows[] = $row; 
-		}
-		$result_n_fieldName['result'] = $db_rows;
-
-		$field_names = array();
-		while($field = $db->get_field($result)) {
-				$field_names[] = $field->name;
-			}
-		// While this assoc 'field_names' contains the column names of the executed sql table
-		$result_n_fieldName['field_names'] = $field_names;
-
-		//$db->close_connection();
-		// so that it can do 2 opearations and return 2 results from one function
-		return $result_n_fieldName;
-	}
-
-	protected static function instanciate($sql) {
-
-		$result_n_fieldName = self::get_column_names($sql);
-
-		// To make it clear I separate the two 
-		$db_rows = $result_n_fieldName['result'];
-		$field_names = $result_n_fieldName['field_names'];
-		
-		$array_of_objects = array();
-		
-		// This will determine the number of objects that has to be created
-		// upon the numbers of rows received from the database
-		for ($i=0, $size = sizeof($db_rows) ; $i < $size; $i++) { 
-
-			$object = new bookObject();
-
-			foreach ($field_names as $field_name) {
-				$object->$field_name = $db_rows[$i][$field_name];
-			}
-			$array_of_objects[] = $object;
-		}
-		// If sql query returns only one row then just returns one object
-		// Else the array of objects 
-		return sizeof($array_of_objects) > 1 ? $array_of_objects : $array_of_objects[0];
-	}
-
 	public static function delete($id) {
 		global $db;
 
