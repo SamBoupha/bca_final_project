62f7a9d10fdcdc745f697a5470cbe2acf51727c5
Sam Boupha <samboupha@yahoo.com>
2015-02-18 16:32:17 +0530

    implemented computer index page

--- public/computers.php
+++ public/computers.php
@@ -3,7 +3,7 @@
 <?php include(INC_PATH.DS."header.php"); ?>
 <?php 
 	require_once(INC_PATH.DS.'computer_object.php'); 
-	$computers = ComputerObject::select(16,true);
+	$computers = ComputerObject::select_all(null,16);
 ?>
 <?php include(INC_PATH.DS."side-nav.php"); ?>
 <section>
@@ -16,15 +16,16 @@
 
 					echo "<a href='computer_detail.php?id=".$computer->id."'>";
 					echo "<img src='img/computer/".$computer->img_thumb."'>";
-					echo "<p class='name'>".$computer->brand." ".$computer->model."</p>";
-					echo "<p class='creator'>by ".$computer->brand."</p>";
+					echo "<p class='name'>".$computer->brand." ".$computer->model;
+					echo "<span class='creator'> by ".$computer->brand."</span></p>";
 
 					echo "<ul>";
 					echo "<li>".$computer->cpu."</li>";
-					echo "<li>".$computer->hdd."</li>";
-					echo "<li>".$computer->ram."</li>";
-					echo "<li>".$computer->mornitor."inch display ".$computer->graphic."</li>";
-					echo "<li>".$computer->os."</li>";
+					echo "<li>HDD ".$computer->hdd."</li>";
+					echo "<li>RAM ".$computer->ram."</li>";
+					echo "<li>".$computer->mornitor." inch display ".$computer->graphic."</li>";
+					echo "<li>".$computer->os."".$computer->architecture."</li>";
+					echo "</ul>";
 
 					echo "</a><p class='price'>Rs. ".$computer->price."</p>";
 
