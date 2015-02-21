c02c9ff700f64b492e58d8322ad64aebb671487d
Sam Boupha <samboupha@yahoo.com>
2015-02-22 01:02:45 +0530

    added place order button

--- public/checkout.php
+++ public/checkout.php
@@ -57,6 +57,7 @@
 				<p>Discount: <span><?php echo "Rs. 0" ?></span></p>
 				<p>Delivery: <span class='success'><?php echo "Free" ?></span></p>
 				<h4>Order total: <span><?php echo "Rs. ".$sum ?></h4>
+				<div class="btn buy-now">PLACE ORDER</div>
 			</div>	
 		</div>
 	</div>
