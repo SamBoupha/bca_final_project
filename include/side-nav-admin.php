5268482818660c6cad38450083d51339a219263d
Sam Boupha <samboupha@yahoo.com>
2015-03-02 16:47:05 +0530

    added link to view-order page in admin-nav

--- include/side-nav-admin.php
+++ include/side-nav-admin.php
@@ -1,5 +1,10 @@
 <nav>
 	<ul>
+		<li>View Orders</li>
+		<ul>
+			<a href='view-order.php?q=computer'><li>Orders on computers</li></a></a>
+			<a href='view-order.php?q=book'><li>Orders on books</li></a>
+		</ul>
 		<a href='../index.php' target='_BLANK'><li>View the public page</li></a>
 		<a href='carousel-edit.php'><li>Edit the slideshow</li></a>
 		<a href='product-add.php'><li>Add new product</li></a>
