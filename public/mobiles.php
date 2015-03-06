795295c03df054f027876976294f517a17eb7498
Sam Boupha <samboupha@yahoo.com>
2015-03-07 00:13:09 +0530

    created mobiles page

---
+++ public/mobiles.php
@@ -0,0 +1,41 @@
+<?php include("../include/initialize.php"); ?>
+<?php $subpage = true; ?>
+<?php include(INC_PATH.DS."header.php"); ?>
+<?php 
+	require_once(INC_PATH.DS.'mobile_object.php'); 
+	$mobiles = mobileObject::select_all($id=null, $limit=16, $public=true, $highlight=false);
+?>
+<?php include(INC_PATH.DS."side-nav.php"); ?>
+<section>
+	<div class='products'>
+		<h1>mobiles &amp; Tablets</h1>
+		<div class='products-display'>
+			<?php
+				foreach ($mobiles as $mobile) {
+					echo "<div class='product-thumbnail'>";
+
+					echo "<a href='mobile_detail.php?id=".$mobile->id."'>";
+					echo "<img src='img/mobile/".$mobile->img_thumb."' style='height:85px'>";
+					echo "<p class='name'>".$mobile->brand." ".$mobile->model;
+					echo "<span class='creator'> by ".$mobile->brand."</span></p>";
+
+					echo "<ul>";
+					echo "<li>".$mobile->cpu."</li>";
+					echo "<li>HDD ".$mobile->hdd."</li>";
+					echo "<li>RAM ".$mobile->ram."</li>";
+					echo "<li>".$mobile->screen." inch display ".$mobile->screen_type."</li>";
+					echo "<li>".$mobile->os."</li>";
+					echo "</ul>";
+
+					echo "</a><p class='price'>Rs. ".number_format($mobile->price)."</p>";
+
+					echo "</div>";
+				}
+			?>
+		</div>
+	</div>		
+	
+</section> <!-- main body section-->
+
+<?php include(INC_PATH.DS."footer.php"); ?>
+		
